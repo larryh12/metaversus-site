7c1137bf886ebb76385c83d59573d0348f39574f
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 03:57:13 +0700

    add gradient mixing between section comps

--- sections/About.jsx
+++ sections/About.jsx
@@ -38,6 +38,14 @@ const About = () => (
         <span className="font-extrabold text-white">explore</span> the madness
         of the metaverse by scrolling down
       </motion.p>
+
+      {/* arrow down */}
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
     </motion.div>
   </section>
 );
