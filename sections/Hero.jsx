30ea11b4ac87df9a35256b69eb6545bae7ec4058
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 02:21:16 +0700

    add parent motion div in hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,23 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    {/* parent motion div to render animation */}
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      // anime not only once but multiple time when scroll to viewport
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      bla
+    </motion.div>
   </section>
 );
 
