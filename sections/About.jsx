40b61052fac91c922cc7658e712f1eebe3755269
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 03:13:32 +0700

    imports for about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,11 @@
 'use client';
 
-const About = () => (
-  <section>
-    About section
-  </section>
-);
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => <section>About section</section>;
 
 export default About;
