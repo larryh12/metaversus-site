61a28b5e89544ff1a4e90761a01f569b834f8e91
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 02:48:15 +0700

    add cover img sliding motion div

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,6 +16,7 @@ const Hero = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
+      {/* metaverse madness title div */}
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
           Metaverse
@@ -25,11 +26,26 @@ const Hero = () => (
           className="flex flex-row justify-center items-center"
         >
           <h1 className={styles.heroHeading}>Ma</h1>
-          {/* bend the div to make the long letter D */}
+          {/* bend the div borders to make the long letter D */}
           <div className={styles.heroDText} />
           <h1 className={styles.heroHeading}>Ness</h1>
         </motion.div>
       </div>
+
+      {/* cover img sliding in div */}
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        {/* round the top left corner of the div img container */}
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]">
+          <img
+            src="cover.png"
+            alt="cover"
+            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+          />
+        </div>
+      </motion.div>
     </motion.div>
   </section>
 );
