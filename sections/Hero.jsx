17e87dc03a65483fc90855f00d25ed18cb2994b7
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 02:57:01 +0700

    fixed cover gradient div bug

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -37,14 +37,13 @@ const Hero = () => (
         variants={slideIn('right', 'tween', 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        {/* round the top left corner of the div img container */}
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]">
-          <img
-            src="cover.png"
-            alt="cover"
-            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-          />
-        </div>
+        {/* this is the gradient div to add bg effect to the cover img */}
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]" />
+        <img
+          src="cover.png"
+          alt="cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
       </motion.div>
     </motion.div>
   </section>
