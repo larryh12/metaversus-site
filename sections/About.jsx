ffa7a8f0d5269e3887f63261f8a73d5970d906d5
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 03:17:06 +0700

    add gradient

--- sections/About.jsx
+++ sections/About.jsx
@@ -6,6 +6,11 @@ import { TypingText } from '../components';
 import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
-const About = () => <section>About section</section>;
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    About section
+  </section>
+);
 
 export default About;
