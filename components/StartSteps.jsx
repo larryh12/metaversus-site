0c9ed8012e755a623c516eadc6de8064e35724d6
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 16:37:30 +0700

    complete startsteps comp

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,3 +1,21 @@
-const StartSteps = () => <div>start steps</div>;
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  // step wrapper div
+  <div className={`${styles.flexCenter} flex-row`}>
+    {/* step number wrapper div */}
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      {/* the actual step number */}
+      <p className="font-bold text-[20px] text-white">{number}</p>
+
+      {/* the step text wrapper div */}
+    </div>
+    <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {text}
+    </p>
+  </div>
+);
 
 export default StartSteps;
