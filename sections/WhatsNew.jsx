b7e4c8c86a7df6b373a07c34de8ab39d2bdf10f0
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 18:36:13 +0700

    setup text wrapper div and add title texts

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -17,7 +17,14 @@ const WhatsNew = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
-      whatsnew
+      {/* upper text wrapper */}
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.95] flex justify-center flex-col"
+      >
+        <TypingText title="| Whats new?" />
+        <TitleText title="What's new about MetaverSUS?" />
+      </motion.div>
     </motion.div>
   </section>
 );
