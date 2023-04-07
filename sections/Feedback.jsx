43a49c95d789b037f9c4631a641aac4638cc4e92
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:38:22 +0700

    feedback section imports

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-const Feedback = () => (
-  <section>
-    Feedback section
-  </section>
-);
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
+const Feedback = () => <section>Feedback section</section>;
 
 export default Feedback;
