478433b20a9ba1152fca6133b572e82fc4a49be6
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 21:05:32 +0700

    footer imports

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,11 @@
 'use client';
 
-const Footer = () => (
-  <footer>
-    footer
-  </footer>
-);
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
+const Footer = () => <footer>footer</footer>;
 
 export default Footer;
