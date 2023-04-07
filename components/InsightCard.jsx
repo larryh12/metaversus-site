5e961fd2f570d7687d4cddfa752be9681d9dbfec
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:13:41 +0700

    add insight thumbnail img

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -8,7 +8,12 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    insight card
+    {/* insight thumbnail */}
+    <img
+      src={imgUrl}
+      alt={`insight-${index}`}
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+    />
   </motion.div>
 );
 
