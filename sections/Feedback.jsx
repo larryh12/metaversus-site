9626fdb0e2cc6efef1d2430d612542db217b2d56
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:40:20 +0700

    section motion wrapper

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -4,6 +4,18 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
-const Feedback = () => <section>Feedback section</section>;
+const Feedback = () => (
+  <section className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+      feedback
+    </motion.div>
+  </section>
+);
 
 export default Feedback;
