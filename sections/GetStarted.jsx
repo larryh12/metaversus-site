2ce08c09fa11088796b0c3e2be186166526b44aa
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 16:09:46 +0700

    get started section imports

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,9 +1,12 @@
 'use client';
 
-const GetStarted = () => (
-  <section>
-    Get Started Section
-  </section>
-);
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { startingFeatures } from '../constants';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+
+const GetStarted = () => <section>Get Started Section</section>;
 
 export default GetStarted;
