1a39f184c270591655605bcd2f0c780154402868
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:43:43 +0700

    add feedback card wrapper

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -13,7 +13,11 @@ const Feedback = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
     >
-      feedback
+      {/* feedback card wrapper */}
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+      ></motion.div>
     </motion.div>
   </section>
 );
