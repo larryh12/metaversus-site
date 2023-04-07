75e8e21051a917a65107aee99d091d98feff5649
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 18:57:21 +0700

    add section text

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,7 +16,12 @@ const World = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      world
+      {/* add texts */}
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        title="Track friends around you and invite them to play together in the same world"
+        textStyles="text-center"
+      />
     </motion.div>
   </section>
 );
