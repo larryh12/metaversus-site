53620159e3c153a5ef867d80b9587694a022a4bf
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 19:11:33 +0700

    insight section imports

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,11 @@
 'use client';
 
-const Insights = () => (
-  <section>
-    Insights section
-  </section>
-);
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { insights } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+const Insights = () => <section>Insights section</section>;
 
 export default Insights;
