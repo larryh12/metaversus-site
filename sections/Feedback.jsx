12ac92c476be71769e8a55e591d40c97400dc075
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:52:28 +0700

    add text content inside feedback card

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -17,7 +17,22 @@ const Feedback = () => (
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
-      ></motion.div>
+      >
+        {/* color gradient div */}
+        <div className="feedback-gradient" />
+        {/* text text text  */}
+        <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+          Samantha
+        </h4>
+        <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+          Founder MetaverSUS
+        </p>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+          “With the development of today's technology, metaverse is very useful
+          for today's work, or can be called web 3.0, by using metaverse you can
+          use it as anything”
+        </p>
+      </motion.div>
     </motion.div>
   </section>
 );
