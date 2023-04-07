e82c2aa0bb335fe50a411b333c13b9a4389fb0fe
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 21:13:06 +0700

    add footer heading text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,25 @@ const Footer = () => (
     <div className="footer-gradient" />
     {/* overall footer text wrapper */}
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      footer
+      {/* footer heading text wrapper */}
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the Metaverse
+        </h4>
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
+          <img
+            src="headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="font-normal text-[16px] text-white">
+            Enter Metaverse
+          </span>
+        </button>
+      </div>
     </div>
   </motion.footer>
 );
