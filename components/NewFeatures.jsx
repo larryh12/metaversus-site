c753206768baec436b6627f740c5fd3d07828dd4
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 18:43:22 +0700

    complete NewFeatures comp

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,22 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  // feature wrapper div
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    {/* feature icon wrapper div */}
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      {/* actual icon img */}
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    {/* below just title and subtitle text */}
+    <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
+      Title {title}
+    </h1>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {subtitle}
+    </p>
   </div>
 );
 
