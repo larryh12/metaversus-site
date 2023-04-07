67b3e247192dcb5884c2e8c469957320e8a863dd
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 14:43:40 +0700

    motion wrapper div

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,6 +8,17 @@ import { exploreWorlds } from '../constants';
 import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
 
-const Explore = () => <section>Explore section</section>;
+const Explore = () => (
+  <section className={`${styles.paddings}`} id="explore">
+    {/* motion wrapper div */}
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    ></motion.div>
+  </section>
+);
 
 export default Explore;
