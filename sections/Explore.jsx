a7d667b3b7ce6c34c964ffcc56435e13a6e4024c
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 15:15:34 +0700

    set initial state for useState

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,34 +8,38 @@ import { exploreWorlds } from '../constants';
 import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
 
-const Explore = () => (
-  <section className={`${styles.paddings}`} id="explore">
-    {/* motion wrapper div */}
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText title="Choose the world you want" textStyles="text-center" />
-      <TitleText title="to explore" textStyles="text-center" />
+const Explore = () => {
+  // set the 2nd world to be active initially
+  const [active, setActive] = useState('world-2');
 
-      {/* world img wrapper */}
-      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
-        {exploreWorlds.map((world, index) => (
-          <ExploreCard
-            key={world.id}
-            {...world}
-            index={index}
-            active={active}
-            handleClick={setActive}
-          />
-        ))}
-      </div>
-    </motion.div>
-  </section>
-);
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      {/* motion wrapper div */}
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText title="Choose the world you want" textStyles="text-center" />
+        <TitleText title="to explore" textStyles="text-center" />
 
+        {/* world img wrapper */}
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 export default Explore;
