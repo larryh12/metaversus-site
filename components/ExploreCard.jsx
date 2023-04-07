0ae02121f061026b7fa8b934ae3b272c88e98de7
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 16:01:05 +0700

    wtf pixel?

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -39,7 +39,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
+        <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
           Enter Metaverse
         </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
