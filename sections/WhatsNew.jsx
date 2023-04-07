4ad8bbf87639f3e9a397ab6cf67dc0df2ae755e4
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 18:27:37 +0700

    whatsnew comp imports

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,9 +1,12 @@
 'use client';
 
-const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
-);
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+
+const WhatsNew = () => <section>What's new section</section>;
 
 export default WhatsNew;
