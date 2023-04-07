475bcef1a1d48b2ed94d8b83a6f61c50e94df899
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 16:12:03 +0700

    add parent motion wrapper div

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -7,6 +7,16 @@ import { startingFeatures } from '../constants';
 import { StartSteps, TitleText, TypingText } from '../components';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 
-const GetStarted = () => <section>Get Started Section</section>;
+const GetStarted = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    ></motion.div>
+  </section>
+);
 
 export default GetStarted;
