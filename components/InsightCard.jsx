66f48e2924fb4f56edb5d941f966a7555843e7a7
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 20:21:01 +0700

    add text wrapper

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -14,6 +14,19 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       alt={`insight-${index}`}
       className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
     />
+
+    {/* text wrapper inclduing arrow */}
+    <div className="w-full flex justify-between items-center">
+      {/* text wrapper without arrow, only title and subtitle */}
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white">
+          {title}
+        </h4>
+        <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
+          {subtitle}
+        </p>
+      </div>
+    </div>
   </motion.div>
 );
 
