1a3d5ce7cbe8c3fd5deb476ea2f2038099b819be
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-07 18:51:58 +0700

    world imports

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,11 @@
 'use client';
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { TitleText, TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const World = () => <section>World section</section>;
 
 export default World;
